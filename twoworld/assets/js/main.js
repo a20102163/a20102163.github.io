4c536035c65883ab0d4c653204f079d2391f115e
Abao <a201021632@gmail.com>
2023-09-06 20:28:16 +0800

    上傳更新檔案

---
+++ twoworld/assets/js/main.js
@@ -0,0 +1,135 @@
+/*=============== SHOW MENU ===============*/
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
+
+  toggle.addEventListener("click", () => {
+    // Add show-menu class to nav menu
+    nav.classList.toggle("show-menu");
+
+    // Add show-icon to show and hide the menu icon
+    toggle.classList.toggle("show-icon");
+  });
+};
+
+showMenu("nav-toggle", "nav-menu");
+
+/*=============== SHOW DROPDOWN MENU ===============*/
+const dropdownItems = document.querySelectorAll(".dropdown__item");
+
+// 1. Select each dropdown item
+dropdownItems.forEach((item) => {
+  const dropdownButton = item.querySelector(".dropdown__button");
+
+  // 2. Select each button click
+  dropdownButton.addEventListener("click", () => {
+    // 7. Select the current show-dropdown class
+    const showDropdown = document.querySelector(".show-dropdown");
+
+    // 5. Call the toggleItem function
+    toggleItem(item);
+
+    // 8. Remove the show-dropdown class from other items
+    if (showDropdown && showDropdown !== item) {
+      toggleItem(showDropdown);
+    }
+  });
+});
+
+// 3. Create a function to display the dropdown
+const toggleItem = (item) => {
+  // 3.1. Select each dropdown content
+  const dropdownContainer = item.querySelector(".dropdown__container");
+
+  // 6. If the same item contains the show-dropdown class, remove
+  if (item.classList.contains("show-dropdown")) {
+    dropdownContainer.removeAttribute("style");
+    item.classList.remove("show-dropdown");
+  } else {
+    // 4. Add the maximum height to the dropdown content and add the show-dropdown class
+    dropdownContainer.style.height = dropdownContainer.scrollHeight + "px";
+    item.classList.add("show-dropdown");
+  }
+};
+
+/*=============== DELETE DROPDOWN STYLES ===============*/
+const mediaQuery = matchMedia("(min-width: 1118px)"),
+  dropdownContainer = document.querySelectorAll(".dropdown__container");
+
+// Function to remove dropdown styles in mobile mode when browser resizes
+const removeStyle = () => {
+  // Validate if the media query reaches 1118px
+  if (mediaQuery.matches) {
+    // Remove the dropdown container height style
+    dropdownContainer.forEach((e) => {
+      e.removeAttribute("style");
+    });
+
+    // Remove the show-dropdown class from dropdown item
+    dropdownItems.forEach((e) => {
+      e.classList.remove("show-dropdown");
+    });
+  }
+};
+
+addEventListener("resize", removeStyle);
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  this.scrollY >= 50
+    ? header.classList.add("bg-header")
+    : header.classList.remove("bg-header");
+};
+window.addEventListener("scroll", scrollHeader);
+
+/*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scrollup class
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+window.addEventListener("scroll", scrollUp);
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionsClass = document.querySelector(
+        ".nav__menu a[href*=" + sectionId + "]"
+      );
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+  // reset: true // Animations repeat
+});
+
+sr.reveal(`.home__img, .newsletter__container, 
+           .footer__logo, .footer__description, 
+           .footer__content, .footer__info`);
+sr.reveal(`.home__data`, { origin: "bottom" });
+sr.reveal(`.about__data, .recently__data`, { origin: "left" });
+sr.reveal(`.about__img, .recently__img`, { origin: "right" });
+sr.reveal(`.popular__card`, { interval: 100 });
